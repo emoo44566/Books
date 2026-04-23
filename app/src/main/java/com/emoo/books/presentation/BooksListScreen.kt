a87f856cad39ab944389c094bc580e578f253d2b
emoo <emoo445666@gmail.com>
2026-04-23 21:02:22 +0330

    feat: add fab to BooksListScreen

--- app/src/main/java/com/emoo/books/presentation/BooksListScreen.kt
+++ app/src/main/java/com/emoo/books/presentation/BooksListScreen.kt
@@ -5,6 +5,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,24 +17,40 @@ import androidx.navigation.NavController
 import com.emoo.books.presentation.components.BookCard
 import com.emoo.books.presentation.components.BookEvent
 import com.emoo.books.presentation.components.SortOptions
+import com.emoo.books.utils.Screen
 
 @Composable
 fun BooksListScreen(navController: NavController, booksViewModel: BooksListViewModel) {
-    Column(Modifier.fillMaxSize()) {
-
-        SortOptions(booksViewModel.sortOrder.value) { order ->
-            booksViewModel.onEvent(BookEvent.Order(order))
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navController.navigate(Screen.BookAddEditScreen)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add"
+                )
+            }
         }
+    ) { contentPadding ->
+        Column(Modifier
+            .fillMaxSize()
+            .padding(contentPadding)) {
 
-        LazyColumn(
-            Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        ) {
-            items(booksViewModel.books.value) { book ->
-                BookCard(book, onDeleteClick = {
-                    booksViewModel.onEvent(BookEvent.Delete(book))
-                })
+            SortOptions(booksViewModel.sortOrder.value) { order ->
+                booksViewModel.onEvent(BookEvent.Order(order))
+            }
+
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                items(booksViewModel.books.value) { book ->
+                    BookCard(book, onDeleteClick = {
+                        booksViewModel.onEvent(BookEvent.Delete(book))
+                    })
+                }
             }
         }
     }
