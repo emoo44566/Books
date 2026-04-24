40247a7d2af3b16f98ec6cd6b572eec77cd1a4b2
emoo <emoo445666@gmail.com>
2026-04-24 14:09:59 +0330

    feat: add addOrUpdateBook function in BookUtils

--- app/src/main/java/com/emoo/books/utils/BookUtils.kt
+++ app/src/main/java/com/emoo/books/utils/BookUtils.kt
@@ -9,39 +9,103 @@ import com.emoo.books.presentation.components.SortByRead
 import com.emoo.books.presentation.components.SortByTitle
 import com.emoo.books.presentation.components.SortOrder
 
-fun getBooks(sortOrder: SortOrder): List<BookVM> {
-    val books = listOf(
-        BookVM("Sapiens", "Yuval Noah Harari", true, NonFiction),
-        BookVM("The Great Gatsby", "F. Scott Fitzgerald", false, Fiction),
-        BookVM("Atomic Habits", "James Clear", true, NonFiction),
-        BookVM("1984", "George Orwell", true, Fiction),
-        BookVM("Becoming", "Michelle Obama", true, NonFiction),
-        BookVM("The Hobbit", "J.R.R. Tolkien", true, Fiction),
-        BookVM("Thinking, Fast and Slow", "Daniel Kahneman", false, NonFiction),
-        BookVM("To Kill a Mockingbird", "Harper Lee", true, Fiction),
-        BookVM("Educated", "Tara Westover", true, NonFiction),
-        BookVM("Pride and Prejudice", "Jane Austen", false, Fiction),
-        BookVM("The Power of Habit", "Charles Duhigg", false, NonFiction),
-        BookVM("The Catcher in the Rye", "J.D. Salinger", false, Fiction),
-        BookVM("Moby-Dick", "Herman Melville", false, Fiction),
-        BookVM("The Diary of a Young Girl", "Anne Frank", true, NonFiction),
-        BookVM("War and Peace", "Leo Tolstoy", false, Fiction),
-        BookVM("Quiet", "Susan Cain", false, NonFiction),
-        BookVM("The Alchemist", "Paulo Coelho", true, Fiction),
-        BookVM("Born a Crime", "Trevor Noah", false, NonFiction),
-        BookVM("The Da Vinci Code", "Dan Brown", true, Fiction),
-        BookVM("Daring Greatly", "Brené Brown", false, NonFiction),
-        BookVM("The Immortal Life of Henrietta Lacks", "Rebecca Skloot", true, NonFiction),
-        BookVM("The Wright Brothers", "David McCullough", false, NonFiction),
-        BookVM("The Art of War", "Sun Tzu", true, NonFiction),
-        BookVM("The 7 Habits of Highly Effective People", "Stephen R. Covey", false, NonFiction)
+val books = mutableListOf(
+    BookVM(title = "Sapiens", author = "Yuval Noah Harari", read = true, bookType = NonFiction),
+    BookVM(
+        title = "The Great Gatsby",
+        author = "F. Scott Fitzgerald",
+        read = false,
+        bookType = Fiction
+    ),
+    BookVM(title = "Atomic Habits", author = "James Clear", read = true, bookType = NonFiction),
+    BookVM(title = "1984", author = "George Orwell", read = true, bookType = Fiction),
+    BookVM(title = "Becoming", author = "Michelle Obama", read = true, bookType = NonFiction),
+    BookVM(title = "The Hobbit", author = "J.R.R. Tolkien", read = true, bookType = Fiction),
+    BookVM(
+        title = "Thinking, Fast and Slow",
+        author = "Daniel Kahneman",
+        read = false,
+        bookType = NonFiction
+    ),
+    BookVM(
+        title = "To Kill a Mockingbird",
+        author = "Harper Lee",
+        read = true,
+        bookType = Fiction
+    ),
+    BookVM(title = "Educated", author = "Tara Westover", read = true, bookType = NonFiction),
+    BookVM(
+        title = "Pride and Prejudice",
+        author = "Jane Austen",
+        read = false,
+        bookType = Fiction
+    ),
+    BookVM(
+        title = "The Power of Habit",
+        author = "Charles Duhigg",
+        read = false,
+        bookType = NonFiction
+    ),
+    BookVM(
+        title = "The Catcher in the Rye",
+        author = "J.D. Salinger",
+        read = false,
+        bookType = Fiction
+    ),
+    BookVM(title = "Moby-Dick", author = "Herman Melville", read = false, bookType = Fiction),
+    BookVM(
+        title = "The Diary of a Young Girl",
+        author = "Anne Frank",
+        read = true,
+        bookType = NonFiction
+    ),
+    BookVM(title = "War and Peace", author = "Leo Tolstoy", read = false, bookType = Fiction),
+    BookVM(title = "Quiet", author = "Susan Cain", read = false, bookType = NonFiction),
+    BookVM(title = "The Alchemist", author = "Paulo Coelho", read = true, bookType = Fiction),
+    BookVM(title = "Born a Crime", author = "Trevor Noah", read = false, bookType = NonFiction),
+    BookVM(title = "The Da Vinci Code", author = "Dan Brown", read = true, bookType = Fiction),
+    BookVM(
+        title = "Daring Greatly",
+        author = "Brené Brown",
+        read = false,
+        bookType = NonFiction
+    ),
+    BookVM(
+        title = "The Immortal Life of Henrietta Lacks",
+        author = "Rebecca Skloot",
+        read = true,
+        bookType = NonFiction
+    ),
+    BookVM(
+        title = "The Wright Brothers",
+        author = "David McCullough",
+        read = false,
+        bookType = NonFiction
+    ),
+    BookVM(title = "The Art of War", author = "Sun Tzu", read = true, bookType = NonFiction),
+    BookVM(
+        title = "The 7 Habits of Highly Effective People",
+        author = "Stephen R. Covey",
+        read = false,
+        bookType = NonFiction
     )
+)
 
+fun getBooks(sortOrder: SortOrder): List<BookVM> {
     return when (sortOrder) {
         is SortByAuthor -> books.sortedBy { it.author }
         is SortByTitle -> books.sortedBy { it.title }
         is SortByRead -> books.sortedBy { it.read }
         is SortByFictional -> books.sortedBy { it.bookType == Fiction }
     }
+}
 
+fun addOrUpdateBook(book: BookVM) {
+    val b = books.find { it.id == book.id }
+    b?.let { books.remove(it) }
+    books.add(book)
+}
+
+fun deleteBook(book: BookVM) {
+    books.remove(book)
 }
