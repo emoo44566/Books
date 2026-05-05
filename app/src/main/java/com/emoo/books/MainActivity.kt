d9f5592d4f8ec4c18b4d4d1bfd0ebb111637db84
emoo <emoo445666@gmail.com>
2026-05-06 00:33:13 +0330

    feat: add navigation parameter to BookAddEditScreen in MainActivity

--- app/src/main/java/com/emoo/books/MainActivity.kt
+++ app/src/main/java/com/emoo/books/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.emoo.books.presentation.addedit.BookAddEditScreen
 import com.emoo.books.presentation.addedit.BookAddEditViewModel
 import com.emoo.books.presentation.list.BooksListScreen
@@ -36,8 +38,17 @@ class MainActivity : ComponentActivity() {
                             val books = viewModel<BooksListViewModel>()
                             BooksListScreen(navController, books)
                         }
-                        composable(Screen.BookAddEditScreen.route) {
-                            val book = viewModel<BookAddEditViewModel>()
+                        composable(
+                            Screen.BookAddEditScreen.route + "?bookId={bookId}",
+                            arguments = listOf(navArgument(name = "bookId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            })
+                        ) { navBackStackEntry ->
+                            val bookId = navBackStackEntry.arguments?.getInt("bookId") ?: -1
+                            val book = viewModel<BookAddEditViewModel>() {
+                                BookAddEditViewModel(bookId)
+                            }
                             BookAddEditScreen(navController, book)
                         }
                     }
